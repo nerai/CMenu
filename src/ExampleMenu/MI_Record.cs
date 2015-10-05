f7976157510e666d84f9b92922d8f776b7e1e4f4
Sebastian Heuchler <git@01h.de>
2015-10-05 19:22:20 +0200

    add record, replay

---
+++ src/ExampleMenu/MI_Record.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ConsoleMenu;
+
+namespace ExampleMenu
+{
+	public class MI_Record : IMenuItem
+	{
+		public string EndrecordCommand = "endrecord";
+
+		public string Selector
+		{
+			get { return "record"; }
+		}
+
+		public string HelpText
+		{
+			get
+			{
+				return "record\n"
+					+ "XXX.";
+			}
+		}
+
+		public MenuResult Execute (string arg)
+		{
+			if (string.IsNullOrWhiteSpace (arg)) {
+				Console.WriteLine ("You must enter a name to identify this command group.");
+				return MenuResult.Normal;
+			}
+
+			var lines = new List<string> ();
+			for (; ; ) {
+				var line = Console.ReadLine ();
+				if (EndrecordCommand.Equals (line)) {
+					break;
+				}
+				lines.Add (line);
+			}
+
+			Directory.CreateDirectory (".\\Records\\");
+			File.WriteAllLines (".\\Records\\" + arg + ".txt", lines);
+
+			return MenuResult.Normal;
+		}
+	}
+}
---
+++ src/ExampleMenu/MI_Replay.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ConsoleMenu;
+
+namespace ExampleMenu
+{
+	public class MI_Replay : IMenuItem
+	{
+		private readonly CMenu _Menu;
+
+		public MI_Replay (CMenu menu)
+		{
+			if (menu == null) {
+				throw new ArgumentNullException ("menu");
+			}
+
+			_Menu = menu;
+		}
+
+		public string Selector
+		{
+			get { return "replay"; }
+		}
+
+		public string HelpText
+		{
+			get
+			{
+				return "replay\n"
+					+ "XXX.";
+			}
+		}
+
+		public MenuResult Execute (string arg)
+		{
+			if (string.IsNullOrWhiteSpace (arg)) {
+				Console.WriteLine ("You must enter a name to identify this command group.");
+				// todo: besser: alle vorhandenen records auflisten
+				return MenuResult.Normal;
+			}
+
+			Directory.CreateDirectory (".\\Records\\");
+			var lines = File.ReadAllLines (".\\Records\\" + arg + ".txt"); // todo: file not found
+			foreach (var line in lines) {
+				_Menu.Input (line, false);
+			}
+
+			return MenuResult.Normal;
+		}
+	}
+}
