268112713d6fa46c16f61a1c51c2d7070956bc17
Sebastian Heuchler <git@01h.de>
2015-10-05 18:06:56 +0200

    display menu

--- src/ExampleMenu/Program.cs
+++ src/ExampleMenu/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ConsoleMenu;
 
 namespace ExampleMenu
 {
@@ -9,6 +10,12 @@ namespace ExampleMenu
 	{
 		static void Main (string[] args)
 		{
+			Console.WriteLine ("Example project for CMenu");
+			var menu = new CMenu ();
+
+			menu.Add (new MI_Echo ());
+
+			menu.Run ();
 		}
 	}
 }
