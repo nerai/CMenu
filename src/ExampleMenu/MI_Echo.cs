ff787514393850102885da5e8c6d601ea79fb108
Sebastian Heuchler <git@01h.de>
2015-10-05 18:06:07 +0200

    add echo command

---
+++ src/ExampleMenu/MI_Echo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleMenu;
+
+namespace ExampleMenu
+{
+	public class MI_Echo : IMenuItem
+	{
+		public string Selector
+		{
+			get { return "echo"; }
+		}
+
+		public string HelpText
+		{
+			get
+			{
+				return "echo [text]\n"
+					+ "Prints the specified text to stdout.";
+			}
+		}
+
+		public MenuResult Execute (string arg)
+		{
+			Console.WriteLine (arg);
+			return MenuResult.Normal;
+		}
+	}
+}
