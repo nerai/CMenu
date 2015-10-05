ac65ff158c0805526cc9045a43b565cb67b5b7a7
Sebastian Heuchler <git@01h.de>
2015-10-05 19:24:35 +0200

    add pause

---
+++ src/ExampleMenu/MI_Pause.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleMenu;
+
+namespace ExampleMenu
+{
+	public class MI_Pause : IMenuItem
+	{
+		public string Selector
+		{
+			get { return "pause"; }
+		}
+
+		public string HelpText
+		{
+			get
+			{
+				return "pause\n"
+					+ "XXX.";
+			}
+		}
+
+		public MenuResult Execute (string arg)
+		{
+			Console.ReadLine ();
+
+			return MenuResult.Normal;
+		}
+	}
+}
