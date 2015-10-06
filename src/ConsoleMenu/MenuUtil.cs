a80b46e11c1abe3b2a79c6cd0d4ac6201a90742f
Sebastian Heuchler <git@01h.de>
2015-10-06 21:18:46 +0200

    ~ menuutil now static ~ add codedoc in menuutil

--- src/ConsoleMenu/MenuUtil.cs
+++ src/ConsoleMenu/MenuUtil.cs
@@ -5,10 +5,23 @@ using System.Text;
 
 namespace ConsoleMenu
 {
-	public class MenuUtil
+	public static class MenuUtil
 	{
+		/// <summary>
+		/// Split the first word, separated by whitespace, from the rest of the string and return it.
+		/// </summary>
+		/// <param name="from">
+		/// String from which to select the first word. This parameter will be changed to exclude the split off word.
+		/// </param>
+		/// <returns>
+		/// First whitespace-separated word in argument.
+		/// </returns>
 		public static string SplitFirstWord (ref string from)
 		{
+			if (from == null) {
+				throw new ArgumentNullException ("from");
+			}
+
 			var split = from.Split (new char[] { ' ', '\t' }, 2, StringSplitOptions.RemoveEmptyEntries);
 			from = split.Length > 1 ? split[1].TrimStart () : "";
 			var word = split.Length > 0 ? split[0].Trim () : "";
