510260c41569405f0011aac345e324c24a159c87
Sebastian Heuchler <git@01h.de>
2015-10-07 20:56:06 +0200

    add StringComparison extensions

---
+++ src/ConsoleMenu/StringComparisonExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleMenu
+{
+	// todo codedoc
+	public static class StringComparisonExtensions
+	{
+		public static bool IsCaseSensitive (this StringComparison sc)
+		{
+			return false
+				|| sc == StringComparison.CurrentCulture
+				|| sc == StringComparison.InvariantCulture
+				|| sc == StringComparison.Ordinal;
+		}
+
+		public static IEqualityComparer<string> GetCorrespondingComparer (this StringComparison sc)
+		{
+			switch (sc) {
+				case StringComparison.CurrentCulture:
+					return StringComparer.CurrentCulture;
+
+				case StringComparison.CurrentCultureIgnoreCase:
+					return StringComparer.CurrentCultureIgnoreCase;
+
+				case StringComparison.InvariantCulture:
+					return StringComparer.InvariantCulture;
+
+				case StringComparison.InvariantCultureIgnoreCase:
+					return StringComparer.InvariantCultureIgnoreCase;
+
+				case StringComparison.Ordinal:
+					return StringComparer.Ordinal;
+
+				case StringComparison.OrdinalIgnoreCase:
+					return StringComparer.OrdinalIgnoreCase;
+
+				default:
+					throw new InvalidOperationException ("Unknown string comparison value.");
+			}
+		}
+	}
+}
