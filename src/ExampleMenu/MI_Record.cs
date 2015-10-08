496212916aa5d32359566866402f71866002d11f
Sebastian Heuchler <git@01h.de>
2015-10-08 02:14:59 +0200

    update MI_Record with 0.2 features

--- src/ExampleMenu/MI_Record.cs
+++ src/ExampleMenu/MI_Record.cs
@@ -7,20 +7,38 @@ using ConsoleMenu;
 
 namespace ExampleMenu
 {
-	public class MI_Record : CMenuItem
+	public class MI_Record : CMenu
 	{
-		public string EndrecordCommand = "endrecord";
+		private List<string> _Lines;
+
+		private string _EndRecordCommand = "endrecord";
+
+		public string EndRecordCommand
+		{
+			get
+			{
+				return _EndRecordCommand;
+			}
+			set
+			{
+				this[_EndRecordCommand].Selector = value;
+				_EndRecordCommand = value;
+			}
+		}
 
 		public MI_Record ()
 			: base ("record")
 		{
 			HelpText = ""
-				+ "record name\n"
+				+ Selector + " name\n"
 				+ "Records all subsequent commands to the specified file name.\n"
-				+ "Recording can be stopped by the command \"" + EndrecordCommand + "\"\n"
+				+ "Recording can be stopped by the command \"" + EndRecordCommand + "\"\n"
 				+ "Stored records can be played via the \"replay\" command.\n"
 				+ "\n"
 				+ "Nested recording is not supported.";
+
+			Add (EndRecordCommand, s => MenuResult.Quit, "Finishes recording.");
+			Add (null, s => _Lines.Add (s));
 		}
 
 		public override MenuResult Execute (string arg)
@@ -30,17 +48,11 @@ namespace ExampleMenu
 				return MenuResult.Normal;
 			}
 
-			var lines = new List<string> ();
-			for (; ; ) {
-				var line = Console.ReadLine ();
-				if (EndrecordCommand.Equals (line)) {
-					break;
-				}
-				lines.Add (line);
-			}
+			_Lines = new List<string> ();
+			Run ();
 
 			Directory.CreateDirectory (".\\Records\\");
-			File.WriteAllLines (".\\Records\\" + arg + ".txt", lines);
+			File.WriteAllLines (".\\Records\\" + arg + ".txt", _Lines);
 
 			return MenuResult.Normal;
 		}
