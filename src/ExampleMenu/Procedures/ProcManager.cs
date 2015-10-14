22aae01e3e006a93443c23dbe87226d07f9e525c
Sebastian Heuchler <git@01h.de>
2015-10-14 11:44:27 +0200

    enable jumping

--- src/ExampleMenu/Procedures/ProcManager.cs
+++ src/ExampleMenu/Procedures/ProcManager.cs
@@ -12,16 +12,28 @@ namespace ExampleMenu.Procedures
 		private class Proc
 		{
 			public readonly List<string> Commands = new List<string> ();
+			public readonly Dictionary<string, int> JumpMarks = new Dictionary<string, int> ();
 
 			public Proc (IEnumerable<string> content)
 			{
 				Commands = new List<string> (content);
+
+				for (int i = 0; i < Commands.Count; i++) {
+					var s = Commands[i];
+					if (s.StartsWith (":")) {
+						s = s.Substring (1);
+						var name = MenuUtil.SplitFirstWord (ref s);
+						JumpMarks[name] = i;
+						Commands[i] = s;
+					}
+				}
 			}
 		}
 
 		private readonly Dictionary<string, Proc> _Procs = new Dictionary<string, Proc> ();
 
 		private bool _RequestReturn = false;
+		private string _RequestJump = null;
 
 		public void AddProc (string name, IEnumerable<string> content)
 		{
@@ -46,12 +58,22 @@ namespace ExampleMenu.Procedures
 					_RequestReturn = false;
 					break;
 				}
+				if (_RequestJump != null) {
+					i = proc.JumpMarks[_RequestJump]; // todo check
+					_RequestJump = null;
+					break;
+				}
 			}
 		}
 
 		public void Return ()
 		{
 			_RequestReturn = true;
+		}
+
+		public void Jump (string mark)
+		{
+			_RequestJump = mark;
 		}
 	}
 }
